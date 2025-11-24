1ede70da213fb1b6f97a49b1e697b426e8e71e58
Andrew Ahn <andrewahn21@gmail.com>
2025-11-24 01:21:57 -0800

    created job script

--- inference.py
+++ inference.py
@@ -21,4 +21,26 @@ with torch.no_grad():
         temperature=0.7,
     )
 
-print(tokenizer.decode(output[0], skip_special_tokens=True))
+result = tokenizer.decode(output[0], skip_special_tokens=True)
+
+with open("/home/taegyoem/scratch/llama2_7b_output.txt", "w") as f:
+    f.write(result)
+
+# import json
+
+# results = []
+
+# prompts = [
+#     "Explain why the sky is blue in one short paragraph.",
+#     "Summarize quantum mechanics in two sentences."
+# ]
+
+# for prompt in prompts:
+#     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+#     with torch.no_grad():
+#         output = model.generate(**inputs, max_new_tokens=150, temperature=0.7)
+#     text = tokenizer.decode(output[0], skip_special_tokens=True)
+#     results.append({"prompt": prompt, "output": text})
+
+# with open("/home/$USER/scratch/llama2_7b_outputs.json", "w") as f:
+#     json.dump(results, f, indent=2)
---
+++ run_inferene.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=llama_infer
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=24G
+#SBATCH --time=00:15:00
+#SBATCH --output=llama_infer_%j.out
+
+# Load Python
+module load StdEnv/2023 python/3.12.4
+
+# Activate your virtual environment (recommended)
+source ~/envs/llama/bin/activate
+
+# Run inference
+python inference.py
