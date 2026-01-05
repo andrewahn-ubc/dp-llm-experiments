734b70218b834b493152120d263acc484ea24656
Andrew Ahn <andrewahn21@gmail.com>
2026-01-05 00:55:56 -0800

    modified to only one pass through dataset

--- run.sh
+++ run.sh
@@ -3,8 +3,9 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
-#SBATCH --time=02:00:00
-#SBATCH --output=gcg_%j.out
+#SBATCH --time=03:00:00
+#SBATCH --array=0-49             # 50 tasks
+#SBATCH --output=gcg_%A_%a.out   # %A = array job ID, %a = task ID
 
 # Load Python
 module load StdEnv/2023 python/3.12.4
