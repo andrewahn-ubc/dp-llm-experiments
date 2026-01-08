6e02262a0d0d5566de4b2dbd478ff64b96288afa
Andrew Ahn <andrewahn21@gmail.com>
2026-01-08 15:17:13 -0800

    install nanogcg

--- run.sh
+++ run.sh
@@ -14,5 +14,7 @@ module load StdEnv/2023 python/3.12.4
 # source ~/envs/llama/bin/activate
 source $SCRATCH/venvs/llama/bin/activate
 
+pip install nanogcg
+
 # Run gcg
 python ~/scratch/dp-llm-experiments/gcg.py
