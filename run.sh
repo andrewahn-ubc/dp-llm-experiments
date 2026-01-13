fdaa6ef19f67d6dc7fdcdcc782a705f0e54d6d13
Andrew Ahn <andrewahn21@gmail.com>
2026-01-13 05:58:31 -0800

    updated job script

--- run.sh
+++ run.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=gcg
+#SBATCH --job-name=train
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=40G
-#SBATCH --time=08:00:00
-#SBATCH --array=0-49             # 50 tasks
-#SBATCH --output=output/gcg_%A_%a.out   # %A = array job ID, %a = task ID
+#SBATCH --time=04:00:00
+#SBATCH --output=output/train.out   
 
 # Load Python
 module load StdEnv/2023 python/3.11
 
 # Activate virtual environment 
-# source ~/envs/llama/bin/activate
 source $SCRATCH/venv/nanogcg/bin/activate
 
-# Run gcg
-python ~/scratch/dp-llm-experiments/gcg.py
+# Run training script
+python ~/scratch/dp-llm-experiments/train.py
