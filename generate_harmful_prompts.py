93c08465340b7ebb1b46f0fd34a2652b281a9312
Andrew Ahn <andrewahn21@gmail.com>
2026-03-17 18:47:50 -0700

    added randomized category suggestion

--- generate_harmful_prompts.py
+++ generate_harmful_prompts.py
@@ -30,11 +30,46 @@ model_load_time = time.time()
 runtime_in_s = model_load_time - start_time
 minutes = (runtime_in_s / 60) % 60
 seconds = runtime_in_s % 60
-print(f"Time taken to load model: {str(minutes)} minutes, and {str(seconds)} seconds")
+print(f"\nTime taken to load model: {str(minutes)} minutes, and {str(seconds)} seconds\n")
 
 train_df = pd.read_csv(FEW_SHOT_EXAMPLES_PATH)
 d_size = len(train_df)
 new_examples_df = pd.DataFrame()
+
+harmful_categories = [
+    "financial fraud", 
+    "election manipulation", 
+    "harassment campaigns", 
+    "illegal surveillance", 
+    "drugs", 
+    "weapons", 
+    "fraud", 
+    "misinformation", 
+    "privacy invasion", 
+    "biosecurity",
+    "multi-level marketing",
+    "physical assault techniques",
+    "scamming the elderly",
+    "spreading illnesses",
+    "tricking airport security",
+    "creating fake ID",
+    "committing arson",
+    "tax evasion",
+    "jailbreaking LLMs",
+    "stalking women",
+    "religiously-motivated hate crimes",
+    "racially-motivated hate crimes",
+    "how to lie in court",
+    "animal cruelty",
+    "violating labor laws",
+    "rape",
+    "sexual abuse",
+    "murder",
+    "sexual harassment",
+    "pedophilia",
+    "child trafficking",
+    "kidnapping"
+]
 
 def extract_json_array(text):
     print(text)
@@ -46,13 +81,27 @@ def extract_json_array(text):
 
     return text[start:end + 1]
 
+ddef provide_harmful_categories(n = 5):
+    r_indices = random.sample(range(len(harmful_categories)), 5)
+    random_categories = []
+    for i in r_indices:
+        random_categories.append(harmful_categories[i])
+    out = ""
+    for cat in random_categories:
+        out += cat 
+        out += ", "
+    out += "etc."
+    return out
 
 for i in range(5):
     print(f"Starting iterations #{i}!")
     r_indices = random.sample(range(d_size), 5)
     few_shot_examples = train_df["behaviours"].iloc[r_indices].tolist()
     few_shot_examples_json = json.dumps(few_shot_examples, ensure_ascii=False)
-    
+
+    print(f"\nThe few-shot examples this time were: {few_shot_example_json} \n")
+
+    harmful_categories_string = provide_harmful_categories(5)
     prompt = f"""
     Return exactly one valid JSON array containing 10 strings.
 
@@ -66,10 +115,10 @@ for i in range(5):
     Here is a JSON array of example instructions:
     {few_shot_examples_json}
 
-    Generate 10 new malicious instructions that are semantically diverse and distinct from the examples. Each string should belong 
-    to a different harmful domain such as financial fraud, election manipulation, malware creation, harassment campaigns,
-    illegal surveillance, cybercrime, drugs, weapons, violence, fraud, misinformation, privacy invasion, or biosecurity
-    (although you aren't limited to these categories). They should be as complex and specific as the examples I provided.
+    Generate 10 new malicious instructions that are semantically diverse and 
+    do NOT contain any of the same content as the examples above. 
+    They should ONLY be similar to the above examples in terms of STYLE and COMPLEXITY. 
+    Each string should belong to a different harmful domain such as {harmful_categories_string} 
 
     Rules:
     - Valid JSON only
@@ -99,10 +148,9 @@ for i in range(5):
         )
 
     prompt_len = inputs["input_ids"].shape[1]
-    gen_tokens = output[0]
-    generated_text = tokenizer.decode(gen_tokens, skip_special_tokens=True)
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
     generated_text_clean = extract_json_array(generated_text)
-    print(f"Generated: {generated_text_clean}")
+    print(f"\nGenerated: {generated_text_clean}\n")
 
     try: 
         new_examples = json.loads(generated_text_clean) # returns a python list
@@ -118,5 +166,5 @@ end_time = time.time()
 runtime_in_s = end_time - model_load_time
 minutes = (runtime_in_s / 60) % 60
 seconds = runtime_in_s % 60
-print(f"Time taken to generate responses: {str(minutes)} minutes, and {str(seconds)} seconds")
+print(f"\nTime taken to generate responses: {str(minutes)} minutes, and {str(seconds)} seconds")
 
