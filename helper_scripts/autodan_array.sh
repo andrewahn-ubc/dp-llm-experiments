559a067aae368625b112dca6fc81eee8c137efb8
Andrew Ahn <andrewahn21@gmail.com>
2026-03-22 15:34:44 -0700

    autodan array job script

---
+++ helper_scripts/autodan_array.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=autodan_array
+#SBATCH --array=0-21
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=40G
+#SBATCH --time=09:00:00
+#SBATCH --output=output/autodan/autodan_%A_%a.out
+
+module load python cuda
+
+source $SCRATCH/venv/autodan/bin/activate
+
+IDX=$(printf "%02d" ${SLURM_ARRAY_TASK_ID})
+DATA_PATH="/home/taegyoem/scratch/dp-llm-experiments/official_data/test_${IDX}.csv"
+
+echo "Running on file: $DATA_PATH"
+
+cd $SCRATCH/AutoDAN
+
+python autodan_hga_eval.py \
+    --input_file "$DATA_PATH" \
+    --save_suffix "dataset_$IDX"
--- helper_scripts/combine_csv.py
+++ helper_scripts/combine_csv.py
@@ -1,12 +1,17 @@
 import sys, pandas as pd
 
-f1, f2 = "./harmbench_training_trimmed.csv", "harmbench_trimmed.csv"
-out = "harmbench.csv"
+f1, f2, f3 = "./eval_advbench.csv", "eval_harmbench.csv", "eval_jbb.csv"
+out = "combined.csv"
 
 df1 = pd.read_csv(f1)
 df2 = pd.read_csv(f2)
+df3 = pd.read_csv(f3)
 
-df = pd.concat([df1, df2], ignore_index=True)
+df1["dataset"] = "advbench"
+df2["dataset"] = "harmbench"
+df3["dataset"] = "jailbreakbench"
+
+df = pd.concat([df1, df2, df3], ignore_index=True)
 df = df[df["goal"].notna()]
 df["goal"] = df["goal"].astype(str).str.strip()
 df = df[df["goal"] != ""]
