4cc6a1a745cd802c2a851f65fb25cda7c19174c2
Taegyoem Ahn <taegyoem@narval2.narval.calcul.quebec>
2026-03-23 20:02:49 -0400

    fix autodan command bug

--- helper_scripts/perturbation/autodan_array.sh
+++ helper_scripts/perturbation/autodan_array.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 #SBATCH --job-name=autodan_array
-#SBATCH --array=0-59
+#SBATCH --array=0-21
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=40G
 #SBATCH --time=09:00:00
-#SBATCH --output=output/autodan/autodan_training_%A_%a.out
+#SBATCH --output=output/autodan/autodan_testing_%A_%a.out
 
 module load python cuda
 
 source $SCRATCH/venv/autodan/bin/activate
 
 IDX=$(printf "%02d" ${SLURM_ARRAY_TASK_ID})
-DATA_PATH="/home/taegyoem/scratch/dp-llm-experiments/official_data/train_${IDX}.csv"
+DATA_PATH="/home/taegyoem/scratch/dp-llm-experiments/official_data/test_${IDX}.csv"
 
 echo "Running on file: $DATA_PATH"
 
 cd $SCRATCH/AutoDAN
 
 python autodan_hga_eval.py \
-    --input_file "$DATA_PATH" \
-    --save_suffix "training_dataset_$IDX"
+    --dataset_path "$DATA_PATH" \
+    --save_suffix "testing_dataset_$IDX"
