90c4bee9835f6a0419adef23c2b759c30eed5e75
Andrew Ahn <andrewahn21@gmail.com>
2026-04-14 00:27:06 -0700

    hyperparam search: lr=5e-5 (again)

--- epoch2.sh
+++ epoch2.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=40G
 #SBATCH --time=9:00:00
-#SBATCH --output=output/lr1_train_epoch_2_%j.out
+#SBATCH --output=output/lr5_train_epoch_2_%j.out
 
 mkdir -p output
 
@@ -31,14 +31,14 @@ mkdir -p $HF_HOME
 # export LLM_NAME=$SLURM_TMPDIR/llama2_7b_chat_hf
 # export GUARD_NAME=$SLURM_TMPDIR/llama_guard_7b
 
-IDX="lr1"
+IDX="lr5"
 
 # Run training
 python $SCRATCH/dp-llm-experiments/train.py \
     --eval-mode "seen-family" \
     --finetuned-llm-path "$SCRATCH/${IDX}_finetuned_llm" \
     --training-data "$SCRATCH/dp-llm-experiments/official_data/train.csv" \
-    --lr 1e-5 \
+    --lr 5e-5 \
     --lambda-val 1.0 \
     --epsilon 0.0 \
     --lora-rank 8 \
